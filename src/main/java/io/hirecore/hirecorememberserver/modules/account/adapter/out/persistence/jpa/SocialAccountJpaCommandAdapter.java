ecdd8ba0971d5c88e1f6ed45a08244de79164ba7
klmeuncheol@kakao.com <klmeuncheol@kakao.com>
2026-03-22 05:07:17 +0900

    feat(account): account 영속성 매퍼 및 어댑터 구현
    - MemberAccountJpaEntityMapper: 도메인↔JPA 엔티티 양방향 매핑
    - SocialAccountJpaEntityMapper: 도메인↔JPA 엔티티 양방향 매핑
    - MemberAccountJpaCommandAdapter: MemberAccountCommandPort 구현, Event Bridge 포함
    - MemberAccountJpaQueryAdapter: MemberAccountQueryPort 구현, readOnly 트랜잭션
    - SocialAccountJpaCommandAdapter: SocialAccountCommandPort 구현, Event Bridge 포함
    - SocialAccountJpaQueryAdapter: SocialAccountQueryPort 구현, readOnly 트랜잭션

---
+++ src/main/java/io/hirecore/hirecorememberserver/modules/account/adapter/out/persistence/jpa/SocialAccountJpaCommandAdapter.java
@@ -0,0 +1,32 @@
+package io.hirecore.hirecorememberserver.modules.account.adapter.out.persistence.jpa;
+
+import io.hirecore.hirecorememberserver.modules.account.adapter.out.persistence.jpa.entity.SocialAccountJpaEntity;
+import io.hirecore.hirecorememberserver.modules.account.adapter.out.persistence.jpa.mapper.SocialAccountJpaEntityMapper;
+import io.hirecore.hirecorememberserver.modules.account.adapter.out.persistence.jpa.repository.SocialAccountJpaCommandRepository;
+import io.hirecore.hirecorememberserver.modules.account.domain.SocialAccount;
+import io.hirecore.hirecorememberserver.modules.account.application.port.out.SocialAccountCommandPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Component
+@RequiredArgsConstructor
+public class SocialAccountJpaCommandAdapter implements SocialAccountCommandPort {
+
+    private final SocialAccountJpaEntityMapper socialAccountJpaEntityMapper;
+    private final SocialAccountJpaCommandRepository socialAccountJpaCommandRepository;
+
+    @Override
+    public SocialAccount save(SocialAccount socialAccount) {
+        SocialAccountJpaEntity entity = socialAccountJpaEntityMapper.toJpaEntity(socialAccount);
+
+        Collection<Object> domainEvents = socialAccount.pollAllEvents();
+        if (domainEvents != null && !domainEvents.isEmpty()) {
+            domainEvents.forEach(entity::recordPersistenceEvent);
+        }
+
+        SocialAccountJpaEntity savedEntity = socialAccountJpaCommandRepository.save(entity);
+        return socialAccountJpaEntityMapper.toDomain(savedEntity);
+    }
+}
---
+++ src/main/java/io/hirecore/hirecorememberserver/modules/account/adapter/out/persistence/jpa/mapper/SocialAccountJpaEntityMapper.java
@@ -0,0 +1,16 @@
+package io.hirecore.hirecorememberserver.modules.account.adapter.out.persistence.jpa.mapper;
+
+import io.hirecore.hirecorememberserver.common.adapter.out.persistence.jpa.mapper.ToJpaEntityMapping;
+import io.hirecore.hirecorememberserver.common.mapper.GlobalMapStructConfig;
+import io.hirecore.hirecorememberserver.modules.account.adapter.out.persistence.jpa.entity.SocialAccountJpaEntity;
+import io.hirecore.hirecorememberserver.modules.account.domain.SocialAccount;
+import org.mapstruct.Mapper;
+
+@Mapper(
+        config = GlobalMapStructConfig.class
+)
+public abstract class SocialAccountJpaEntityMapper {
+    @ToJpaEntityMapping
+    public abstract SocialAccountJpaEntity toJpaEntity(SocialAccount domain);
+    public abstract SocialAccount toDomain(SocialAccountJpaEntity jpaEntity);
+}

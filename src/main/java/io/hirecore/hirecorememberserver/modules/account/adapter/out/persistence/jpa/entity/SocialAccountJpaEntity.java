b7779b5ebe773051d5cbdf2eb146b1a979c1a427
klmeuncheol@kakao.com <klmeuncheol@kakao.com>
2026-03-22 04:57:28 +0900

    feat(account): account JPA 엔티티 정의
    - MemberAccountJpaEntity: member_accounts 테이블 매핑, Aggregate Root
    - SocialAccountJpaEntity: social_accounts 테이블 매핑, 복합 유니크 제약조건

---
+++ src/main/java/io/hirecore/hirecorememberserver/modules/account/adapter/out/persistence/jpa/entity/SocialAccountJpaEntity.java
@@ -0,0 +1,57 @@
+package io.hirecore.hirecorememberserver.modules.account.adapter.out.persistence.jpa.entity;
+
+import io.hirecore.hirecorememberserver.common.adapter.out.persistence.jpa.AbstractPersistableAggregateRoot;
+import io.hirecore.hirecorememberserver.common.adapter.out.persistence.jpa.vo.AuditingJpaInfo;
+import io.hirecore.hirecorememberserver.sharedkernel.vo.OAuth2Provider;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.Comment;
+
+import java.time.Instant;
+
+@Entity
+@Table(
+        name="social_accounts",
+        uniqueConstraints = @UniqueConstraint(
+                columnNames = {"provider", "provider_id", "email"}
+        ))
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@SuperBuilder
+@Getter
+public class SocialAccountJpaEntity extends AbstractPersistableAggregateRoot<Long> {
+    @Id
+    private Long id;
+
+    @Comment("연동된 회원 계정 ID")
+    @Column(name="member_account_id", nullable = false)
+    private Long memberAccountId;
+
+    @Comment("소셜 로그인 제공자")
+    @Enumerated(EnumType.STRING)
+    @Column(name="provider", nullable = false, columnDefinition = "VARCHAR(20)")
+    private OAuth2Provider provider;
+
+    @Comment("소셜 사용자 식별 아이디")
+    @Column(name="provider_id", updatable = false, nullable = false)
+    private String providerId;
+
+    @Comment("소셜 계정 이메일")
+    @Column(name="email", nullable = false)
+    private String email;
+
+    @Comment("소셜 로그인 서비스 연동 일자")
+    private Instant connectedAt;
+
+    @Comment("이메일 동의여부")
+    @Column(name="email_agreed", nullable = false)
+    private Boolean emailAgreed;
+
+    @Comment("프로필 닉네임 동의여부")
+    @Column(name="profile_nickname_agreed", nullable = false)
+    private Boolean profileNicknameAgreed;
+
+    @Embedded
+    private AuditingJpaInfo auditingInfo;
+}

0261d537d070b40a3d2329dcaadc04a3090dbd68
klmeuncheol@kakao.com <klmeuncheol@kakao.com>
2026-03-22 18:24:54 +0900

    fix(spring-boot): @ConfigurationPropertiesScan 누락 보정

--- src/main/java/io/hirecore/hirecorememberserver/HirecoreMemberServerApplication.java
+++ src/main/java/io/hirecore/hirecorememberserver/HirecoreMemberServerApplication.java
@@ -3,10 +3,17 @@ package io.hirecore.hirecorememberserver;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan({
+		"io.hirecore.hirecorememberserver.common.properties",
+		"io.hirecore.hirecorememberserver.common.adapter.in.security.properties",
+		"io.hirecore.hirecorememberserver.modules.account.adapter.out.oauth2.kakao.properties",
+		"io.hirecore.hirecorememberserver.common.adapter.out.jwt.properties",
+})
 public class HirecoreMemberServerApplication {
 
 	@PostConstruct

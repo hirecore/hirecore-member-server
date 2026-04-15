d57651153336cde7eb71196843d8d874f191d249
euncheol-kim <klmeuncheol@kakao.com>
2026-04-15 18:56:45 +0900

    feat(file): ImageFileMeta 비즈니스의 도메인 예외처리를 위한 코드 구현 #86
    구현목록
     1. ImageFileMetaDomainException
     2. ImageFileMetaDomainExceptionCodeCluster

---
+++ src/main/java/io/hirecore/hirecorememberserver/modules/file/domain/exception/ImageFileMetaDomainException.java
@@ -0,0 +1,10 @@
+package io.hirecore.hirecorememberserver.modules.file.domain.exception;
+
+import io.hirecore.hirecorememberserver.common.domain.exception.BaseDomainException;
+import io.hirecore.hirecorememberserver.common.domain.exception.DomainExceptionCode;
+
+public class ImageFileMetaDomainException extends BaseDomainException {
+    public ImageFileMetaDomainException(DomainExceptionCode domainExceptionCode) {
+        super("ImageFileMetaDomainException", domainExceptionCode);
+    }
+}

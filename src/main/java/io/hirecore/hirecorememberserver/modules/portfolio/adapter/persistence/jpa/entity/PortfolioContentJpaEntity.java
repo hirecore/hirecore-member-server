62531e6ce8406bfff072d94512af272e26db990b
euncheol-kim <klmeuncheol@kakao.com>
2026-04-16 22:15:30 +0900

    fix(portfolio): @MapsId와 같이 쓰던 @Comment 삭제 및 누락 AuditingJpaInfo 기입 #88

--- src/main/java/io/hirecore/hirecorememberserver/modules/portfolio/adapter/persistence/jpa/entity/PortfolioContentJpaEntity.java
+++ src/main/java/io/hirecore/hirecorememberserver/modules/portfolio/adapter/persistence/jpa/entity/PortfolioContentJpaEntity.java
@@ -1,6 +1,7 @@
 package io.hirecore.hirecorememberserver.modules.portfolio.adapter.persistence.jpa.entity;
 
 import io.hirecore.hirecorememberserver.common.adapter.out.persistence.jpa.AbstractPersistableEntity;
+import io.hirecore.hirecorememberserver.common.adapter.out.persistence.jpa.vo.AuditingJpaInfo;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.Comment;
@@ -16,7 +17,6 @@ public class PortfolioContentJpaEntity extends AbstractPersistableEntity<Long> {
     private Long id;
 
     @MapsId
-    @Comment("포트폴리오 ID")
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "portfolio_id", nullable = false, unique = true)
     private PortfolioJpaEntity portfolio;
@@ -28,6 +28,9 @@ public class PortfolioContentJpaEntity extends AbstractPersistableEntity<Long> {
     @Comment("포트폴리오 본문 HTML 콘텐츠")
     @Column(name = "content_html", nullable = false, columnDefinition = "TEXT")
     private String contentHtml;
+
+    @Embedded
+    private AuditingJpaInfo auditingInfo;
 
     void setPortfolio(PortfolioJpaEntity portfolio) {
         this.portfolio = portfolio;

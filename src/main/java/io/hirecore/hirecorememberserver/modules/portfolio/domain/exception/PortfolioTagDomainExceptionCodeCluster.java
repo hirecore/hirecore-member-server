c2f30ab728374dba334958253bc8014889519a33
euncheol-kim <klmeuncheol@kakao.com>
2026-04-18 00:32:15 +0900

    feat(portfolio): PortfolioTag 도메인 예외처리 클래스 생성 #93

---
+++ src/main/java/io/hirecore/hirecorememberserver/modules/portfolio/domain/exception/PortfolioTagDomainExceptionCodeCluster.java
@@ -0,0 +1,51 @@
+package io.hirecore.hirecorememberserver.modules.portfolio.domain.exception;
+
+import io.hirecore.hirecorememberserver.common.domain.exception.DomainExceptionCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+public class PortfolioTagDomainExceptionCodeCluster {
+    @Getter
+    @RequiredArgsConstructor
+    public enum HiddenDetailResponse implements DomainExceptionCode {
+        // ID
+        ID_MISSING(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "PortfolioTag 도메인 객체 생성에서 id 필드가 누락되었습니다.",
+                "서버에서 오류가 발생했습니다. 관리자에게 문의해주세요."),
+
+        // PORTFOLIO_ID
+        PORTFOLIO_ID_MISSING(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "PortfolioTag 도메인 객체 생성에서 portfolioId 필드가 누락되었습니다.",
+                "서버에서 오류가 발생했습니다. 관리자에게 문의해주세요."),
+
+        // USER_INPUT_TAG
+        USER_INPUT_TAG_MISSING(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "PortfolioTag 도메인 객체 생성에서 userInputTag 필드가 누락되었습니다.",
+                "서버에서 오류가 발생했습니다. 관리자에게 문의해주세요."),
+
+        // NORMALIZED_TAG
+        NORMALIZED_TAG_MISSING(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "PortfolioTag 도메인 객체 생성에서 normalizedTag 필드가 누락되었습니다.",
+                "서버에서 오류가 발생했습니다. 관리자에게 문의해주세요."),
+
+        // AUDITING_INFO
+        AUDITING_INFO_MISSING(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "PortfolioTag 도메인 객체 생성에서 auditingInfo 필드가 누락되었습니다.",
+                "서버에서 오류가 발생했습니다. 관리자에게 문의해주세요.");
+
+        private final HttpStatus httpStatus;
+        private final String logMessage;
+        private final String clientMessage;
+
+        @Override
+        public String getErrorCode() {
+            return this.toString();
+        }
+    }
+}

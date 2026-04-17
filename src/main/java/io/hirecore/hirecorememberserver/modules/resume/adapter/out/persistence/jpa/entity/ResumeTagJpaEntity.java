4d1f39c9d6af42168881a24e9d41e455c381e295
euncheol-kim <klmeuncheol@kakao.com>
2026-04-18 00:24:42 +0900

    feat(resume): ResumeTag JPA 데이터 모델 생성 #92

---
+++ src/main/java/io/hirecore/hirecorememberserver/modules/resume/adapter/out/persistence/jpa/entity/ResumeTagJpaEntity.java
@@ -0,0 +1,43 @@
+package io.hirecore.hirecorememberserver.modules.resume.adapter.out.persistence.jpa.entity;
+
+import io.hirecore.hirecorememberserver.common.adapter.out.persistence.jpa.AbstractPersistableEntity;
+import io.hirecore.hirecorememberserver.common.adapter.out.persistence.jpa.vo.AuditingJpaInfo;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "resume_tags")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+public class ResumeTagJpaEntity extends AbstractPersistableEntity<Long> {
+    @Id
+    private Long id;
+
+    @Comment("이력서 ID")
+    @Column(name = "resume_id", nullable = false)
+    private Long resumeId;
+
+    @Comment("사용자 입력 태그")
+    @Column(name = "user_input_tag", nullable = false, columnDefinition = "VARCHAR(50)")
+    private String userInputTag;
+
+    @Comment("정규화된 태그")
+    @Column(name = "normalized_tag", nullable = false, columnDefinition = "VARCHAR(50)")
+    private String normalizedTag;
+
+    @Comment("삭제 여부")
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted;
+
+    @Comment("삭제 시각")
+    @Column(name = "deleted_at", columnDefinition = "DATETIME(6)")
+    private Instant deletedAt;
+
+    @Embedded
+    private AuditingJpaInfo auditingInfo;
+}

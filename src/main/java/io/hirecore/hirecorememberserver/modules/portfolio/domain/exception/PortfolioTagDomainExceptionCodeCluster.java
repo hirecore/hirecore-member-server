9a7f76e92316fa913a28134e068458323d92c24c
euncheol-kim <klmeuncheol@kakao.com>
2026-04-18 22:31:46 +0900

    feat(portfolio): Portfolio에 나만보기 메모, 포트폴리오 태그 필드 추가 및 PortfolioTag 관계 설정 수정 (#108)
    * refactor(portfolio): PortfolioTag 예외처리 코드에서 PORTFOLIO_ID_MISSING 제거 #107
    
    * refactor(portfolio): PortfolioTag 도메인 모델에서 portfolioId 식별 참조 제거 #107
    
    * refactor(portfolio): PortfolioTagJpaEntity에 @ManyToOne 관계 설정 및 portfolioId 제거 #107
    
    * feat(portfolio): Portfolio 도메인 모델에 secretNote, portfolioTags 필드 추가 #107
    
    * feat(portfolio): PortfolioJpaEntity에 secretNote 필드 추가 #107

--- src/main/java/io/hirecore/hirecorememberserver/modules/portfolio/domain/exception/PortfolioTagDomainExceptionCodeCluster.java
+++ src/main/java/io/hirecore/hirecorememberserver/modules/portfolio/domain/exception/PortfolioTagDomainExceptionCodeCluster.java
@@ -15,12 +15,6 @@ public class PortfolioTagDomainExceptionCodeCluster {
                 "PortfolioTag 도메인 객체 생성에서 id 필드가 누락되었습니다.",
                 "서버에서 오류가 발생했습니다. 관리자에게 문의해주세요."),
 
-        // PORTFOLIO_ID
-        PORTFOLIO_ID_MISSING(
-                HttpStatus.INTERNAL_SERVER_ERROR,
-                "PortfolioTag 도메인 객체 생성에서 portfolioId 필드가 누락되었습니다.",
-                "서버에서 오류가 발생했습니다. 관리자에게 문의해주세요."),
-
         // USER_INPUT_TAG
         USER_INPUT_TAG_MISSING(
                 HttpStatus.INTERNAL_SERVER_ERROR,

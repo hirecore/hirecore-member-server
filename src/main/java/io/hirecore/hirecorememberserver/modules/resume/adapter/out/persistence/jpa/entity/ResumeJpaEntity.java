9eefc5be313a31526bbc1ec00184e1361fa524b7
euncheol-kim <klmeuncheol@kakao.com>
2026-04-18 23:00:59 +0900

    feat(resume): ResumeJpaEntity 임시 생성 #112

---
+++ src/main/java/io/hirecore/hirecorememberserver/modules/resume/adapter/out/persistence/jpa/entity/ResumeJpaEntity.java
@@ -0,0 +1,16 @@
+package io.hirecore.hirecorememberserver.modules.resume.adapter.out.persistence.jpa.entity;
+
+import io.hirecore.hirecorememberserver.common.adapter.out.persistence.jpa.AbstractPersistableAggregateRoot;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "resumes")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+public class ResumeJpaEntity extends AbstractPersistableAggregateRoot<Long> {
+    @Id
+    private Long id;
+}

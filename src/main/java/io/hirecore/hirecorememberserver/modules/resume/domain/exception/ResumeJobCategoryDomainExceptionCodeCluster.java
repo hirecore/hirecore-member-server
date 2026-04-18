86b351f79a6debde0dbc021bb0290416246c1d36
euncheol-kim <klmeuncheol@kakao.com>
2026-04-18 23:01:25 +0900

    refactor(resume): ResumeJobCategory 예외처리 코드에서 RESUME_ID_MISSING 제거 #112

--- src/main/java/io/hirecore/hirecorememberserver/modules/resume/domain/exception/ResumeJobCategoryDomainExceptionCodeCluster.java
+++ src/main/java/io/hirecore/hirecorememberserver/modules/resume/domain/exception/ResumeJobCategoryDomainExceptionCodeCluster.java
@@ -15,12 +15,6 @@ public class ResumeJobCategoryDomainExceptionCodeCluster {
                 "ResumeJobCategory 도메인 객체 생성에서 id 필드가 누락되었습니다.",
                 "서버에서 오류가 발생했습니다. 관리자에게 문의해주세요."),
 
-        // RESUME_ID
-        RESUME_ID_MISSING(
-                HttpStatus.INTERNAL_SERVER_ERROR,
-                "ResumeJobCategory 도메인 객체 생성에서 resumeId 필드가 누락되었습니다.",
-                "서버에서 오류가 발생했습니다. 관리자에게 문의해주세요."),
-
         // JOB_CATEGORY_ID
         JOB_CATEGORY_ID_MISSING(
                 HttpStatus.INTERNAL_SERVER_ERROR,

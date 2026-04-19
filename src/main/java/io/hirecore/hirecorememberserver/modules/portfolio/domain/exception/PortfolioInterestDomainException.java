e989ea1d8629323cad479c482cb03506b6f12ce3
euncheol-kim <klmeuncheol@kakao.com>
2026-04-19 21:57:00 +0900

    refactor(portfolio): PortfolioInterest 도메인 모델 필드 재설계 #122

---
+++ src/main/java/io/hirecore/hirecorememberserver/modules/portfolio/domain/exception/PortfolioInterestDomainException.java
@@ -0,0 +1,10 @@
+package io.hirecore.hirecorememberserver.modules.portfolio.domain.exception;
+
+import io.hirecore.hirecorememberserver.common.domain.exception.BaseDomainException;
+import io.hirecore.hirecorememberserver.common.domain.exception.DomainExceptionCode;
+
+public class PortfolioInterestDomainException extends BaseDomainException {
+    public PortfolioInterestDomainException(DomainExceptionCode domainExceptionCode) {
+        super("PortfolioInterestDomainException", domainExceptionCode);
+    }
+}

f75f861628fe94ae6f56edc6ceb1bf7da165a5c3
euncheol-kim <klmeuncheol@kakao.com>
2026-04-19 21:57:24 +0900

    feat(portfolio): PortfolioMemberView JPA 데이터 모델 생성 #122

---
+++ src/main/java/io/hirecore/hirecorememberserver/modules/portfolio/adapter/persistence/jpa/entity/PortfolioMemberViewJpaEntity.java
@@ -0,0 +1,34 @@
+package io.hirecore.hirecorememberserver.modules.portfolio.adapter.persistence.jpa.entity;
+
+import io.hirecore.hirecorememberserver.common.adapter.out.persistence.jpa.AbstractPersistableEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+import java.time.Instant;
+
+@Entity
+@Table(
+        name = "portfolio_member_views",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"portfolio_id", "member_account_id"})
+)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+public class PortfolioMemberViewJpaEntity extends AbstractPersistableEntity<Long> {
+    @Id
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "portfolio_id", nullable = false)
+    private PortfolioJpaEntity portfolio;
+
+    @Comment("조회한 회원 계정 ID")
+    @Column(name = "member_account_id", nullable = false)
+    private Long memberAccountId;
+
+    @Comment("조회 시각")
+    @Column(name = "viewed_at", nullable = false, columnDefinition = "DATETIME(6)")
+    private Instant viewedAt;
+}

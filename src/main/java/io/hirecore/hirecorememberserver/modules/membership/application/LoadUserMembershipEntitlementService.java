415610fc5f8e0487db081acfd29cb5f94d359acd
euncheol-kim <klmeuncheol@kakao.com>
2026-04-24 21:14:01 +0900

    fix(membership): 스토리지 할당량 조회 결과를 정상 반환하도록 수정

--- src/main/java/io/hirecore/hirecorememberserver/modules/membership/application/LoadUserMembershipEntitlementService.java
+++ src/main/java/io/hirecore/hirecorememberserver/modules/membership/application/LoadUserMembershipEntitlementService.java
@@ -11,7 +11,6 @@ public class LoadUserMembershipEntitlementService {
     private final LoadUserMembershipEntitlementPort loadUserMembershipEntitlement;
 
     public Long loadStorageQuotaBytesSnapshot(Long memberAccountId){
-        loadUserMembershipEntitlement.getStorageQuotaBytesSnapshot(memberAccountId);
-        return null;
+        return loadUserMembershipEntitlement.getStorageQuotaBytesSnapshot(memberAccountId);
     }
 }

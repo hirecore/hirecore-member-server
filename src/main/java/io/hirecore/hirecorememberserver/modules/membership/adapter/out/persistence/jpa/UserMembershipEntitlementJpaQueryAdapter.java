b92994c558ef4f8d8f84798f39f60f824f1b3d61
euncheol-kim <klmeuncheol@kakao.com>
2026-04-30 13:08:06 +0900

    refactor(membership): out persistence 어댑터 네이밍을 CQRS 그룹 컨벤션에 정렬 #149

--- src/main/java/io/hirecore/hirecorememberserver/modules/membership/adapter/out/persistence/jpa/UserMembershipEntitlementJpaQueryAdapter.java
+++ src/main/java/io/hirecore/hirecorememberserver/modules/membership/adapter/out/persistence/jpa/UserMembershipEntitlementJpaQueryAdapter.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class LoadUserMembershipEntitlementJpaAdapter implements LoadUserMembershipEntitlementPort {
+public class UserMembershipEntitlementJpaQueryAdapter implements LoadUserMembershipEntitlementPort {
 
     private final UserMembershipEntitlementJpaQueryRepository userMembershipEntitlementJpaQueryRepository;
 

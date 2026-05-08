f54babaddaa468e8710b2d1243dd3fa857f6c676
euncheol-kim <klmeuncheol@kakao.com>
2026-05-08 17:33:21 +0900

    test(storage): ImageUploaded 이벤트 핸들러 단위 테스트 추가 #180

---
+++ src/test/java/io/hirecore/hirecorememberserver/modules/storage/adapter/in/event/ImageUploadedEventListenerTest.java
@@ -0,0 +1,40 @@
+package io.hirecore.hirecorememberserver.modules.storage.adapter.in.event;
+
+import io.hirecore.hirecorememberserver.modules.storage.application.port.in.RecordImageStorageUsageUseCase;
+import io.hirecore.hirecorememberserver.modules.storage.domain.vo.ResourceKind;
+import io.hirecore.hirecorememberserver.sharedkernel.domain.event.ImageUploadedEvent;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Instant;
+
+import static org.mockito.BDDMockito.then;
+
+@DisplayName("ImageUploadedEventListener 단위 테스트")
+@ExtendWith(MockitoExtension.class)
+class ImageUploadedEventListenerTest {
+
+    @InjectMocks
+    private ImageUploadedEventListener listener;
+
+    @Mock
+    private RecordImageStorageUsageUseCase recordImageStorageUsageUseCase;
+
+    @Test
+    @DisplayName("ImageUploadedEvent를 수신하면 PORTFOLIO_EDITOR_IMAGE 리소스 종류로 사용량 기록 use case를 호출한다")
+    void should_invoke_record_use_case_with_portfolio_editor_image_kind() {
+        // given
+        ImageUploadedEvent event = new ImageUploadedEvent(100L, 1L, 1_048_576L, Instant.now());
+
+        // when
+        listener.on(event);
+
+        // then
+        then(recordImageStorageUsageUseCase).should()
+                .execute(1L, 100L, ResourceKind.PORTFOLIO_EDITOR_IMAGE, 1_048_576L);
+    }
+}

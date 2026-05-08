fbe98376d99f34dc76cde827f1c9c2e2b1bfa46f
euncheol-kim <klmeuncheol@kakao.com>
2026-05-08 18:56:57 +0900

    feat(sharedkernel): DomainType / Purpose 분류 VO를 sharedkernel에 신설 #193

---
+++ src/main/java/io/hirecore/hirecorememberserver/sharedkernel/domain/vo/Purpose.java
@@ -0,0 +1,32 @@
+package io.hirecore.hirecorememberserver.sharedkernel.domain.vo;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import io.hirecore.hirecorememberserver.sharedkernel.domain.exception.SharedKernelException;
+import io.hirecore.hirecorememberserver.sharedkernel.domain.exception.SharedKernelExceptionCodeCluster.HiddenDetailResponse;
+
+public enum Purpose {
+    CONTENT_IMAGE("contentImage"),
+    THUMBNAIL_IMAGE("thumbnailImage");
+
+    private final String value;
+
+    Purpose(String value) {
+        this.value = value;
+    }
+
+    @JsonCreator
+    public static Purpose from(String value) {
+        for (Purpose purpose : values()) {
+            if (purpose.value.equalsIgnoreCase(value)) {
+                return purpose;
+            }
+        }
+        throw new SharedKernelException(HiddenDetailResponse.PURPOSE_INVALID);
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+}

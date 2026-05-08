1709d0cbc8ca02275bb8b463d4035d1fae12e303
euncheol-kim <klmeuncheol@kakao.com>
2026-05-08 18:27:11 +0900

    refactor(portfolio): 영속화 어댑터의 매퍼 의존성을 도메인 객체별 매퍼로 분리 #189

--- src/main/java/io/hirecore/hirecorememberserver/modules/portfolio/adapter/out/persistence/jpa/PortfolioJpaCommandAdapter.java
+++ src/main/java/io/hirecore/hirecorememberserver/modules/portfolio/adapter/out/persistence/jpa/PortfolioJpaCommandAdapter.java
@@ -4,7 +4,10 @@ import io.hirecore.hirecorememberserver.modules.portfolio.adapter.out.persistenc
 import io.hirecore.hirecorememberserver.modules.portfolio.adapter.out.persistence.jpa.entity.PortfolioJobCategoryJpaEntity;
 import io.hirecore.hirecorememberserver.modules.portfolio.adapter.out.persistence.jpa.entity.PortfolioJpaEntity;
 import io.hirecore.hirecorememberserver.modules.portfolio.adapter.out.persistence.jpa.entity.PortfolioTagJpaEntity;
+import io.hirecore.hirecorememberserver.modules.portfolio.adapter.out.persistence.jpa.mapper.PortfolioContentJpaEntityMapper;
+import io.hirecore.hirecorememberserver.modules.portfolio.adapter.out.persistence.jpa.mapper.PortfolioJobCategoryJpaEntityMapper;
 import io.hirecore.hirecorememberserver.modules.portfolio.adapter.out.persistence.jpa.mapper.PortfolioJpaEntityMapper;
+import io.hirecore.hirecorememberserver.modules.portfolio.adapter.out.persistence.jpa.mapper.PortfolioTagJpaEntityMapper;
 import io.hirecore.hirecorememberserver.modules.portfolio.adapter.out.persistence.jpa.repository.PortfolioJobCategoryJpaCommandRepository;
 import io.hirecore.hirecorememberserver.modules.portfolio.adapter.out.persistence.jpa.repository.PortfolioJpaCommandRepository;
 import io.hirecore.hirecorememberserver.modules.portfolio.adapter.out.persistence.jpa.repository.PortfolioTagJpaCommandRepository;
@@ -19,25 +22,28 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PortfolioJpaCommandAdapter implements SavePortfolioPort {
 
-    private final PortfolioJpaEntityMapper mapper;
+    private final PortfolioJpaEntityMapper portfolioMapper;
+    private final PortfolioContentJpaEntityMapper portfolioContentMapper;
+    private final PortfolioJobCategoryJpaEntityMapper portfolioJobCategoryMapper;
+    private final PortfolioTagJpaEntityMapper portfolioTagMapper;
     private final PortfolioJpaCommandRepository portfolioRepository;
     private final PortfolioJobCategoryJpaCommandRepository portfolioJobCategoryRepository;
     private final PortfolioTagJpaCommandRepository portfolioTagRepository;
 
     @Override
     public Portfolio save(Portfolio portfolio) {
-        PortfolioJpaEntity portfolioEntity = mapper.toJpaEntity(portfolio);
-        PortfolioContentJpaEntity contentEntity = mapper.toContentJpaEntity(portfolio.getPortfolioContent());
+        PortfolioJpaEntity portfolioEntity = portfolioMapper.toJpaEntity(portfolio);
+        PortfolioContentJpaEntity contentEntity = portfolioContentMapper.toJpaEntity(portfolio.getPortfolioContent());
         portfolioEntity.syncPortfolioContent(contentEntity);
 
         PortfolioJpaEntity savedPortfolio = portfolioRepository.save(portfolioEntity);
 
-        PortfolioJobCategoryJpaEntity jobCategoryEntity = mapper.toJobCategoryJpaEntity(portfolio.getPortfolioJobCategory());
+        PortfolioJobCategoryJpaEntity jobCategoryEntity = portfolioJobCategoryMapper.toJpaEntity(portfolio.getPortfolioJobCategory());
         jobCategoryEntity.attachPortfolio(savedPortfolio);
         portfolioJobCategoryRepository.save(jobCategoryEntity);
 
         List<PortfolioTagJpaEntity> tagEntities = portfolio.getPortfolioTags().stream()
-                .map(mapper::toTagJpaEntity)
+                .map(portfolioTagMapper::toJpaEntity)
                 .toList();
         for (PortfolioTagJpaEntity tagEntity : tagEntities) {
             tagEntity.attachPortfolio(savedPortfolio);

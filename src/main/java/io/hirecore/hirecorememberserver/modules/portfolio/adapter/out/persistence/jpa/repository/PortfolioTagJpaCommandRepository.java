e99c9b94b70e2e1fc3bf6d23ddb67d4393a598b8
euncheol-kim <klmeuncheol@kakao.com>
2026-05-08 18:41:11 +0900

    refactor(portfolio): 미사용 자식 엔티티 Repository 제거 #191

--- src/main/java/io/hirecore/hirecorememberserver/modules/portfolio/adapter/out/persistence/jpa/repository/PortfolioTagJpaCommandRepository.java
+++
@@ -1,8 +0,0 @@
-package io.hirecore.hirecorememberserver.modules.portfolio.adapter.out.persistence.jpa.repository;
-
-import io.hirecore.hirecorememberserver.modules.portfolio.adapter.out.persistence.jpa.entity.PortfolioTagJpaEntity;
-import org.springframework.data.repository.Repository;
-
-public interface PortfolioTagJpaCommandRepository extends Repository<PortfolioTagJpaEntity, Long> {
-    PortfolioTagJpaEntity save(PortfolioTagJpaEntity portfolioTag);
-}

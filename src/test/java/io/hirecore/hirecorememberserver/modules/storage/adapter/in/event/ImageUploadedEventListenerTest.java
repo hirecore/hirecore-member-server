ac0ded7a2318f8363167727ee617d4b656a154a3
euncheol-kim <klmeuncheol@kakao.com>
2026-05-08 19:12:59 +0900

    refactor(file,storage): ImageUploadedEvent에 도메인 분류 정보 추가 및 리스너에서 ResourceKind 매핑 #195 (#196)
    * refactor(file,sharedkernel): ImageUploadedEvent에 도메인 분류 정보 추가 #195
    
    * refactor(storage): ImageUploadedEventListener에 도메인 분류 기반 ResourceKind 매핑 도입 #195

--- src/test/java/io/hirecore/hirecorememberserver/modules/storage/adapter/in/event/ImageUploadedEventListenerTest.java
+++ src/test/java/io/hirecore/hirecorememberserver/modules/storage/adapter/in/event/ImageUploadedEventListenerTest.java
@@ -3,6 +3,8 @@ package io.hirecore.hirecorememberserver.modules.storage.adapter.in.event;
 import io.hirecore.hirecorememberserver.modules.storage.application.port.in.RecordImageStorageUsageUseCase;
 import io.hirecore.hirecorememberserver.modules.storage.domain.vo.ResourceKind;
 import io.hirecore.hirecorememberserver.sharedkernel.domain.event.ImageUploadedEvent;
+import io.hirecore.hirecorememberserver.sharedkernel.domain.vo.DomainType;
+import io.hirecore.hirecorememberserver.sharedkernel.domain.vo.Purpose;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,17 +26,51 @@ class ImageUploadedEventListenerTest {
     @Mock
     private RecordImageStorageUsageUseCase recordImageStorageUsageUseCase;
 
-    @Test
-    @DisplayName("ImageUploadedEvent를 수신하면 PORTFOLIO_CONTENT 리소스 종류로 사용량 기록 use case를 호출한다")
-    void should_invoke_record_use_case_with_portfolio_content_kind() {
-        // given
-        ImageUploadedEvent event = new ImageUploadedEvent(100L, 1L, 1_048_576L, Instant.now());
+    private static ImageUploadedEvent eventFor(DomainType domainType, Purpose purpose) {
+        return new ImageUploadedEvent(100L, 1L, domainType, purpose, 1_048_576L, Instant.now());
+    }
 
-        // when
+    @Test
+    @DisplayName("PORTFOLIO + CONTENT_IMAGE 이벤트는 PORTFOLIO_CONTENT 리소스로 기록한다")
+    void should_resolve_portfolio_content() {
+        ImageUploadedEvent event = eventFor(DomainType.PORTFOLIO, Purpose.CONTENT_IMAGE);
+
         listener.on(event);
 
-        // then
         then(recordImageStorageUsageUseCase).should()
                 .execute(1L, 100L, ResourceKind.PORTFOLIO_CONTENT, 1_048_576L);
+    }
+
+    @Test
+    @DisplayName("PORTFOLIO + THUMBNAIL_IMAGE 이벤트는 PORTFOLIO_THUMBNAIL 리소스로 기록한다")
+    void should_resolve_portfolio_thumbnail() {
+        ImageUploadedEvent event = eventFor(DomainType.PORTFOLIO, Purpose.THUMBNAIL_IMAGE);
+
+        listener.on(event);
+
+        then(recordImageStorageUsageUseCase).should()
+                .execute(1L, 100L, ResourceKind.PORTFOLIO_THUMBNAIL, 1_048_576L);
+    }
+
+    @Test
+    @DisplayName("RESUME + CONTENT_IMAGE 이벤트는 RESUME_CONTENT 리소스로 기록한다")
+    void should_resolve_resume_content() {
+        ImageUploadedEvent event = eventFor(DomainType.RESUME, Purpose.CONTENT_IMAGE);
+
+        listener.on(event);
+
+        then(recordImageStorageUsageUseCase).should()
+                .execute(1L, 100L, ResourceKind.RESUME_CONTENT, 1_048_576L);
+    }
+
+    @Test
+    @DisplayName("RESUME + THUMBNAIL_IMAGE 이벤트는 RESUME_ATTACHMENT 리소스로 기록한다")
+    void should_resolve_resume_attachment() {
+        ImageUploadedEvent event = eventFor(DomainType.RESUME, Purpose.THUMBNAIL_IMAGE);
+
+        listener.on(event);
+
+        then(recordImageStorageUsageUseCase).should()
+                .execute(1L, 100L, ResourceKind.RESUME_ATTACHMENT, 1_048_576L);
     }
 }

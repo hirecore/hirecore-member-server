5f802572273749981615113141272ab3c7406932
euncheol-kim <klmeuncheol@kakao.com>
2026-05-14 13:40:54 +0900

    refactor(portfolio): portfolio web 레이어에 API 전용 ApiValue 타입 도입 #215

---
+++ src/main/java/io/hirecore/hirecorememberserver/modules/portfolio/adapter/in/web/dto/value/CollaborationTypeApiValue.java
@@ -0,0 +1,30 @@
+package io.hirecore.hirecorememberserver.modules.portfolio.adapter.in.web.dto.value;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum CollaborationTypeApiValue {
+    TEAM("team"),
+    PERSONAL("personal");
+
+    private final String value;
+
+    CollaborationTypeApiValue(String value) {
+        this.value = value;
+    }
+
+    @JsonCreator
+    public static CollaborationTypeApiValue from(String value) {
+        for (CollaborationTypeApiValue v : values()) {
+            if (v.value.equalsIgnoreCase(value)) {
+                return v;
+            }
+        }
+        throw new IllegalArgumentException("Unknown CollaborationType: " + value);
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+}

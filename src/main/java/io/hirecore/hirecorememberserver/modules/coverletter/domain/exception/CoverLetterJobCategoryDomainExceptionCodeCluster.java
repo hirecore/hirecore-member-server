6544981c6ed8ce204de0012868387fb75c77300f
euncheol-kim <klmeuncheol@kakao.com>
2026-06-02 17:08:17 +0900

    refactor(coverletter): CoverLetterJobCategory/CoverLetterTag 자식 엔티티의 soft-delete 필드 제거 #251

--- src/main/java/io/hirecore/hirecorememberserver/modules/coverletter/domain/exception/CoverLetterJobCategoryDomainExceptionCodeCluster.java
+++ src/main/java/io/hirecore/hirecorememberserver/modules/coverletter/domain/exception/CoverLetterJobCategoryDomainExceptionCodeCluster.java
@@ -21,12 +21,6 @@ public class CoverLetterJobCategoryDomainExceptionCodeCluster {
                 "CoverLetterJobCategory 도메인 객체 생성에서 jobCategoryId 필드가 누락되었습니다.",
                 "서버에서 오류가 발생했습니다. 관리자에게 문의해주세요."),
 
-        // IS_DELETED
-        IS_DELETED_MISSING(
-                HttpStatus.INTERNAL_SERVER_ERROR,
-                "CoverLetterJobCategory 도메인 객체 생성에서 isDeleted 필드가 누락되었습니다.",
-                "서버에서 오류가 발생했습니다. 관리자에게 문의해주세요."),
-
         // CONNECTED_AT
         CONNECTED_AT_MISSING(
                 HttpStatus.INTERNAL_SERVER_ERROR,
